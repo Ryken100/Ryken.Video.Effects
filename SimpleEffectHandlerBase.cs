06b870166a0d6d5cc62dc06694674a43ad6266b5
Ryken100 <recarlo100@gmail.com>
2019-10-04 16:42:50 -0400

    Added SimpleEffectHandlerBase

---
+++ SimpleEffectHandlerBase.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Graphics.Canvas.Effects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ryken.Video.Effects
+{
+    public abstract class SimpleEffectHandlerBase<T> : IVideoEffectHandler where T : ICanvasEffect, new()
+    {
+        T effect;
+        void IVideoEffectHandler.CreateResources()
+        {
+            effect = new T();
+        }
+
+        void IVideoEffectHandler.DestroyResources()
+        {
+            effect.Dispose();
+        }
+
+        void IVideoEffectHandler.ProcessFrame(IVideoEffectHandlerArgs args)
+        {
+            SetEffectProperties(args, effect);
+            using (var session = args.OutputFrame.CreateDrawingSession())
+            {
+                session.DrawImage(effect, args.OutputFrame.Bounds, args.InputFrame.Bounds);
+            }
+        }
+        protected abstract void SetEffectProperties(IVideoEffectHandlerArgs args, T effect);
+    }
+}
